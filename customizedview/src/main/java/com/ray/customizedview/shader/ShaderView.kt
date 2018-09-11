1e360a9b325afec1be484dab4d31f99666e62686
hikobe8 <hikobe8@163.com>
2018-09-11 10:47:09 +0800

    Shader.TileMode

--- customizedview/src/main/java/com/ray/customizedview/shader/ShaderView.kt
+++ customizedview/src/main/java/com/ray/customizedview/shader/ShaderView.kt
@@ -16,38 +16,53 @@ class CommonShaderView(context: Context?, attrs: AttributeSet?) : View(context, 
 
     private var mShaderPaint = Paint()
     private var mTextPaint = Paint()
-    private var mRadius = 0f
-    private var mTextForDraw:String = "CLAMP"
+    private var mTextForDraw = ""
     private var mTileMode = Shader.TileMode.CLAMP
+    private var mTextStartX = 0f
+    private lateinit var mRectF: RectF
+    private var mTextBaseLine = 0f
 
     init {
-        val array = context?.obtainStyledAttributes(attrs, R.styleable.ShaderView, 0, 0)
-        mTextForDraw = array?.getString(R.styleable.ShaderView_tile_mode)!!
-        var type = array?.getInteger(R.styleable.PathView_fillType, 0)
-        array.recycle()
+        val array = context?.obtainStyledAttributes(attrs, R.styleable.CommonShaderView, 0, 0)
+        val type = array?.getInteger(R.styleable.CommonShaderView_tile_mode, 0)
+        when (type) {
+            Shader.TileMode.CLAMP.ordinal -> mTileMode =  Shader.TileMode.CLAMP
+            Shader.TileMode.REPEAT.ordinal -> mTileMode = Shader.TileMode.REPEAT
+            Shader.TileMode.MIRROR.ordinal -> mTileMode = Shader.TileMode.MIRROR
+        }
+        mTextForDraw = "Shader.TileMode." + mTileMode.name
+        array?.recycle()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        val size = MeasureSpec.getSize(widthMeasureSpec)
+        setMeasuredDimension(size, size/2)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        mRadius = w/4f
-        var shader = LinearGradient(mRadius,mRadius, mRadius*3, mRadius*3, Color.parseColor("#E91E63"),
-                Color.parseColor("#2196F3"), Shader.TileMode.CLAMP )
-        mShaderPaint.shader = shader
-        mShaderPaint.isAntiAlias = true
-
+        val halfRectWidth = w/8f
         mTextPaint.isAntiAlias = true
         mTextPaint.color = Color.BLACK
         mTextPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 18f, context.resources.displayMetrics)
+        val textBounds = Rect()
+        mTextPaint.getTextBounds(mTextForDraw, 0, mTextForDraw.length, textBounds)
+        val textWidth = textBounds.width()
+        val fontMetrics = mTextPaint.fontMetrics
+        val textHeight = fontMetrics.descent - fontMetrics.ascent
+        mTextBaseLine = textHeight -fontMetrics.top
+        mTextStartX = w/2f - textWidth/2f
+        mRectF = RectF(0f, textHeight*3f, w*1f, h*1f)
+        var shader = LinearGradient(halfRectWidth * 3f, textHeight*3f, halfRectWidth*5, textHeight*3f, Color.parseColor("#E91E63"),
+                Color.parseColor("#2196F3"), mTileMode )
+        mShaderPaint.shader = shader
+        mShaderPaint.isAntiAlias = true
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawText(mTextForDraw, mRadius, mRadius - 20f, mTextPaint)
-        canvas?.drawCircle(mRadius*2,mRadius*2, mRadius, mShaderPaint)
+        canvas?.drawText(mTextForDraw, mTextStartX, mTextBaseLine, mTextPaint)
+        canvas?.drawRect(mRectF, mShaderPaint)
     }
 
 }
