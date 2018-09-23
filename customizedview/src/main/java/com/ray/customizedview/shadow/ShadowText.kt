73e0bf4cf49dced21bef7a732f1d9025cf17520f
hikobe8 <hikobe8@163.com>
2018-09-23 23:31:49 +0800

    complete attributes of ShadowTextView

--- customizedview/src/main/java/com/ray/customizedview/shadow/ShadowText.kt
+++ customizedview/src/main/java/com/ray/customizedview/shadow/ShadowText.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
+import com.ray.customizedview.R
 
 /**
  * Author : hikobe8@github.com
@@ -15,18 +16,44 @@ import android.view.View
  */
 class ShadowText(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-    var mPaint = Paint()
-    val mText = "Hello With Shadow"
+    private var mPaint = Paint()
+    private var mText = ""
+
+    private var mBaseLine: Float
+
+    private var mShadowOffsetX: Float
+
+    private var mShadowOffsetY: Float
+
 
     init {
-        mPaint.textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 24f, context?.resources?.displayMetrics)
-        mPaint.color = Color.BLACK
-        mPaint.setShadowLayer(10f, 0f,0f, Color.RED)
+        val array = context?.obtainStyledAttributes(attrs, R.styleable.ShadowText)
+        mText = array?.getString(R.styleable.ShadowText_text)!!
+        val textColor = array.getColor(R.styleable.ShadowText_textColor, Color.BLACK)
+        val shadowColor = array.getColor(R.styleable.ShadowText_shadowColor, Color.BLACK)
+        val textSize = array.getDimension(R.styleable.ShadowText_textSize,
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16f, context?.resources?.displayMetrics))
+        val shadowRadius = array.getFloat(R.styleable.ShadowText_shadowRadius, 10f)
+        mShadowOffsetX = array.getFloat(R.styleable.ShadowText_shadowOffsetX, 0f)
+        mShadowOffsetY = array.getFloat(R.styleable.ShadowText_shadowOffsetY, 0f)
+        array.recycle()
+        mPaint.isAntiAlias = true
+        mPaint.textSize = textSize
+        mPaint.color = textColor
+        mPaint.setShadowLayer(shadowRadius, mShadowOffsetX,mShadowOffsetY, shadowColor)
+        val fontMetrics = mPaint.fontMetrics
+        mBaseLine = 0 + paddingTop - fontMetrics.top
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width:Int = (mPaint.measureText(mText) + 0.5f + paddingLeft + paddingRight + mShadowOffsetX).toInt()
+        val height: Int = (mPaint.fontMetrics.bottom - mPaint.fontMetrics.top + paddingTop + paddingBottom + mShadowOffsetY).toInt()
+        setMeasuredDimension(width, height)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawText(mText, 0, mText.length, width/4f, height/2f, mPaint)
+        canvas?.drawText(mText,0f, mBaseLine, mPaint)
     }
 
 }
