e5380793e147181d2ff7bffeef9ca0d86c1461b1
hikobe8 <hikobe8@163.com>
2018-10-23 18:02:47 +0800

    perspectiveMatrix

--- reopengles/src/main/java/com/ray/reopengles/TriangleRenderer.java
+++ reopengles/src/main/java/com/ray/reopengles/TriangleRenderer.java
@@ -2,6 +2,7 @@ package com.ray.reopengles;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -19,8 +20,9 @@ public class TriangleRenderer implements GLSurfaceView.Renderer {
 
     private static final String VERTEX_SHADER_CODE =
             "attribute vec4 vPosition;\n"
+                    + "uniform mat4 uMVPMatrix;\n"
                     + "void main() {\n"
-                    + "  gl_Position = vPosition;\n"
+                    + "  gl_Position = uMVPMatrix * vPosition;\n"
                     + "}";
 
     private static final String FRAGMENT_SHADER_CODE =
@@ -31,16 +33,20 @@ public class TriangleRenderer implements GLSurfaceView.Renderer {
 
     private float[] mCoords = {
             0.f, 0.5f, 0f,
-            -0.5f, 0.f, 0f,
-            0.5f, 0f, 0f
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f
     };
+
+    private float[] mMvpMatrix = new float[16];
 
     private FloatBuffer mVertexBuffer;
     private int mProgram;
     //glsl中的vPosition的程序句柄
     private int mPositionHandle;
+    //glsl中的uMVPMatrix的程序句柄
+    private int mMatrixHandle;
 
-    private int loadShader(int type, String code){
+    private int loadShader(int type, String code) {
         int shader = GLES20.glCreateShader(type);
         GLES20.glShaderSource(shader, code);
         GLES20.glCompileShader(shader);
@@ -49,7 +55,7 @@ public class TriangleRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0f, 0f, 0f,0f);
+        GLES20.glClearColor(0f, 0f, 0f, 0f);
         //1创建顶点坐标
         mVertexBuffer = ByteBuffer
                 .allocateDirect(4 * mCoords.length)
@@ -72,16 +78,20 @@ public class TriangleRenderer implements GLSurfaceView.Renderer {
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
         GLES20.glEnableVertexAttribArray(mPositionHandle);
         GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false, 12, mVertexBuffer);
+        mMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+        Matrix.perspectiveM(mMvpMatrix, 0, 45, width*1f/height, 0.1f, 100f);
+        Matrix.translateM(mMvpMatrix, 0, 0f, 0f, -3.5f);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUniformMatrix4fv(mMatrixHandle, 1, false, mMvpMatrix, 0);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
     }
 }
