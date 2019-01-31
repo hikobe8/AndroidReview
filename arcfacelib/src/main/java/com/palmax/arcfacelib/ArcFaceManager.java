33d206cb528b09a0346caf0d1e696481ceddc460
hikobe8 <hikobe8@163.com>
2019-02-01 00:58:27 +0800

    add load path by reflection

--- arcfacelib/src/main/java/com/palmax/arcfacelib/ArcFaceManager.java
+++ arcfacelib/src/main/java/com/palmax/arcfacelib/ArcFaceManager.java
@@ -2,6 +2,7 @@ package com.palmax.arcfacelib;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
@@ -9,6 +10,11 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import dalvik.system.PathClassLoader;
 
 /***
  *  Author : ryu18356@gmail.com
@@ -21,19 +27,51 @@ public class ArcFaceManager {
      * 初始化location，主要做一些拷贝.so的操作
      */
     @SuppressLint("UnsafeDynamicallyLoadedCode")
-    public static void init(Context context) {
+    public static void init(Context context) throws Exception {
+        createNewNativeDir(context);
         File soFile1;
-        soFile1 = new File(context.getDir("jniLibs", Context.MODE_PRIVATE), "libarcsoft_face_engine.so");
+        soFile1 = new File(context.getDir("libs", Context.MODE_PRIVATE), "libarcsoft_face.so");
         File soFile2;
-        soFile2 = new File(context.getDir("jniLibs", Context.MODE_PRIVATE), "libarcsoft_face.so");
+//        soFile2 = new File(context.getDir("libs", Context.MODE_PRIVATE), "libarcsoft_face.so");
         if (soFile1.exists()) {
             System.load(soFile1.getAbsolutePath());
-        } else if (soFile2.exists()) {
-            System.load(soFile2.getAbsolutePath());
-        } else {
-            copySoFileToLocal(context, soFile1, soFile2);
+        }
+//        else if (soFile2.exists()) {
+//            System.load(soFile2.getAbsolutePath());
+//        }
+        else {
+            copySoFileToLocal(context, soFile1/*, soFile2*/);
         }
     }
+
+    private static void createNewNativeDir(Context context) throws Exception {
+        PathClassLoader pathClassLoader = (PathClassLoader) context.getClassLoader();
+        Field declaredField = Class.forName("dalvik.system.BaseDexClassLoader").getDeclaredField("pathList");
+        declaredField.setAccessible(true);
+        Object pathList = declaredField.get(pathClassLoader);
+        // 获取当前类的属性
+        Object nativeLibraryDirectories = pathList.getClass().getDeclaredField(
+                "nativeLibraryDirectories");
+        ((Field) nativeLibraryDirectories).setAccessible(true);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            // 获取 DEXPATHList中的属性值
+            File[] files = (File[]) ((Field) nativeLibraryDirectories).get(pathList);
+            Object filesss = Array.newInstance(File.class, files.length + 1);
+            // 添加自定义.so路径
+            Array.set(filesss, 0, context.getDir("libs", Context.MODE_PRIVATE));
+            // 将系统自己的追加上
+            for (int i = 1; i < files.length + 1; i++) {
+                Array.set(filesss, i, files[i - 1]);
+            }
+            ((Field) nativeLibraryDirectories).set(pathList, filesss);
+        } else {
+            ArrayList<File> files = (ArrayList<File>) ((Field) nativeLibraryDirectories).get(pathList);
+            ArrayList<File> filesss = (ArrayList<File>) files.clone();
+            filesss.add(0, context.getDir("libs", Context.MODE_PRIVATE));
+            ((Field) nativeLibraryDirectories).set(pathList, filesss);
+        }
+    }
+
 
     /**
      * 拷贝.so文件 * *
