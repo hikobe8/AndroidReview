11008e9bc4991a3570f090c1cd43ca3911612382
hikobe8 <hikobe8@163.com>
2019-02-02 00:21:52 +0800

    ImageMatrix

--- imagematrix/src/main/java/com/example/imagematrix/MainActivity.kt
+++ imagematrix/src/main/java/com/example/imagematrix/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.imagematrix
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Matrix
+import android.graphics.*
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
@@ -18,13 +15,16 @@ class MainActivity : AppCompatActivity() {
             if (iv.height > 0) {
                 val viewWidth = iv.width
                 val viewHeight = iv.height
-                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.bg)
+                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.small)
                 val result = Bitmap.createBitmap(iv.width, iv.height, Bitmap.Config.ARGB_4444)
                 val canvas = Canvas(result)
-                val scale = Math.max(viewWidth.toDouble()/bitmap.width, viewHeight.toDouble()/bitmap.height)
+                val scale = Math.max(viewWidth.toDouble() / bitmap.width, viewHeight.toDouble() / bitmap.height)
                 val mt = Matrix().apply {
-                    postTranslate(iv.width /2f- bitmap.width/2f, iv.height/2f - bitmap.height/2f)
-                    postScale(scale.toFloat(), scale.toFloat(),result.width/2f, result.height/2f)
+//                                        postTranslate(iv.width /2f- bitmap.width/2f, iv.height/2f - bitmap.height/2f)
+//                    postScale(scale.toFloat(), scale.toFloat(),result.width/2f, result.height/2f)
+                    setRectToRect(RectF(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat()),
+                            RectF(0f, 0f, result.width.toFloat(), result.height.toFloat())
+                            , Matrix.ScaleToFit.CENTER)
                 }
                 canvas.drawBitmap(bitmap, mt, null)
                 iv.scaleType = ImageView.ScaleType.MATRIX
