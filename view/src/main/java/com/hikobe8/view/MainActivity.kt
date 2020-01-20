227675b28274bbd32827a3ee54e57a8b1f2aec3f
hikobe8 <hikobe8@163.com>
2020-01-20 11:11:13 +0800

    push code

---
+++ view/src/main/java/com/hikobe8/view/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.hikobe8.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun dashboard(view: View) {
+        DashboardActivity.launch(this)
+    }
+
+}
